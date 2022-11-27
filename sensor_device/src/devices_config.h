57fc28ab413fadf039e39c7e587a55d6d80f1740
Zygfryd Homonto <46562447+homonto@users.noreply.github.com>
2022-11-27 12:44:03 +0000

    DEVICE_ID == 4 added

--- sensor_device/src/devices_config.h
+++ sensor_device/src/devices_config.h
@@ -45,7 +45,7 @@
   #define SLEEP_TIME_S                300    // seconds, 300 = 5min
   uint8_t FixedMACAddress[] =         {0x1a, 0x01, 0x01, 0x01, 0x01, 0x02};
 
-  #pragma message "compilation for: DEVICE_ID=1, ESP32-S2, " HOSTNAME
+  #pragma message "compilation for: DEVICE_ID=2, ESP32-S2, " HOSTNAME
 // ---------------------------------------------------------------------------------------------------
 
 #elif DEVICE_ID == 3
@@ -70,7 +70,32 @@
   #define SLEEP_TIME_S                300    // seconds, 300 = 5min
   uint8_t FixedMACAddress[] =         {0x1a, 0x01, 0x01, 0x01, 0x01, 0x03};
 
-  #pragma message "compilation for: DEVICE_ID=1, ESP32-C3, " HOSTNAME
+  #pragma message "compilation for: DEVICE_ID=3, ESP32-C3, " HOSTNAME
+// ---------------------------------------------------------------------------------------------------
+
+#elif DEVICE_ID == 4
+  #define HOSTNAME                    "homekit-sensor-4-TEST"
+  #define BOARD_TYPE                  2     // 1 = ESP32-S, 2 = ESP32-S2, 3 = ESP32-S3, 4 = ESP32-C3
+
+  #define ACT_BLUE_LED_GPIO           6     // comment out if not in use - don't use "0" here
+  #define ERROR_RED_LED_GPIO          5     // comment out if not in use - don't use "0" here
+  #define FW_UPGRADE_GPIO             4   // comment out if not in use - don't use "0" here - cannot be 8 or 9 on new boards if I2C used
+  #define ENABLE_3V_GPIO              3     // comment out if not in use - don't use "0" here - mandatory for I2C devices on new boards
+  #define CUSTOM_SDA_GPIO             8     // override default SDA pin
+  #define CUSTOM_SCL_GPIO             9     // override default SCL pin
+  #define POWER_GPIO                  38  // GREEN, STDB PIN6 on TP4056, LOW on CHARGED (LED ON),       comment out if not in use - don't use "0" here
+  #define CHARGING_GPIO               39  // RED,   CHRG PIN7 on TP4056, LOW during CHARGING (LED ON),  comment out if not in use - don't use "0" here
+  
+
+  #define USE_MAX17048                1     // use "0" to disable
+  #define USE_SHT31                   1     // use "0" to disable
+  #define USE_TSL2561                 1     // use "0" to disable
+  #define LOW_BATTERY_VOLTS           3.7   // low battery warning
+  #define MINIMUM_VOLTS               3.3   // device is going to forced 12 hour sleep to conserve battery
+  #define SLEEP_TIME_S                5    // seconds, 300 = 5min
+  uint8_t FixedMACAddress[] =         {0x1a, 0x01, 0x01, 0x01, 0x01, 0x04};
+
+  #pragma message "compilation for: DEVICE_ID=4, ESP32-S2, " HOSTNAME
 // ---------------------------------------------------------------------------------------------------
 
 #else
